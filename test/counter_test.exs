744e655d86a5357072562556ac6b42de07c99d88
Michael Pope <mpope.cr@gmail.com>
2017-05-12 14:15:33 -0700

    Add test to increment by 50

--- test/counter_test.exs
+++ test/counter_test.exs
@@ -2,6 +2,12 @@ defmodule CounterTest do
   use ExUnit.Case, async: true
   doctest Counter
 
+  def increment(pid, iterations) do
+    for _i <- (1..iterations) do
+      :ok = Counter.increment_by_1(pid)
+    end
+  end
+
   setup do
     {:ok, counter} = Counter.start_link()
     {:ok, [counter: counter]}
@@ -10,8 +16,16 @@ defmodule CounterTest do
   test "incrementing the count by 1", c do
     counter = c[:counter]
     base_count = Counter.read_count(counter)
-    :ok = Counter.increment_by_1(counter)
+    _ = increment(counter, 1)
     new_count = Counter.read_count(counter)
     assert new_count == base_count + 1
+  end
+
+  test "incrementing the count by 50", c do
+    counter = c[:counter]
+    base_count = Counter.read_count(counter)
+    _ = increment(counter, 50)
+    new_count = Counter.read_count(counter)
+    assert new_count == base_count + 50
   end
 end
